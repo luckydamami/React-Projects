912dd7901cb8b39ed4082fc3ad42cc1c071e36e9
luckydamami <luckydamamii2410@gmail.com>
2026-02-17 12:16:56 +0530

    second commit is done

---
+++ 02BgChanger/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="btn-container">
+        <button className="color" id="red">
+          Red
+        </button>
+        <button className="color" id="orange">
+          Orange
+        </button>
+        <button className="color" id="green">
+          Green
+        </button>
+        <button className="color" id="blue">
+          Blue
+        </button>
+        <button className="color" id="black">
+          Black
+        </button>
+        <button className="color" id="yellow">
+          Yellow
+        </button>
+        <button className="color" id="pink">
+          Pink
+        </button>
+        <button className="color" id="purple">
+          Purple
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
